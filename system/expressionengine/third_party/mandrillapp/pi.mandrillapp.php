4376b75cd32bcdaa1d64c1e8d83e020f65938ce0
Gianfranco Montoya <gms122@gmail.com>
2014-05-29 12:47:10 -0500

    email viva_close

--- system/expressionengine/third_party/mandrillapp/pi.mandrillapp.php
+++ system/expressionengine/third_party/mandrillapp/pi.mandrillapp.php
@@ -86,6 +86,61 @@ Atentamente';
 		return "";
 	}
 	
+	function send_email_viva_close(){
+		global $TMPL;
+		$this->EE =& get_instance(); // EEv2 syntax
+		$TMPL = $this->EE->TMPL;
+
+ 		require_once 'mailchimp-mandrill-api-php/src/Mandrill.php'; 
+ 		$mandrill = new Mandrill('Svqgcw575OLrORu2WiD09g');
+ 		
+ 		$to= $TMPL->fetch_param('to');
+ 		$name= $TMPL->fetch_param('name');
+ 		$subject= 'Reporte de Arreglo y Cierre de Caso - Viva GyM';
+ 		$from= $TMPL->fetch_param('from');
+ 		$id_sol_garantia = $TMPL->fetch_param('id_sol_garantia');
+ 		//$text = $TMPL->tagdata;
+ 		$text = 'Estimado/a '.$screen_name.'<p>
+	Gracias por culminar el proceso de atención de su solicitud número: '.$id_sol_garantia.'.
+Adjunto encontrará el reporte del arreglo realizado por GyM.<p>
+Aprovechamos para recordarle que en el Manual del Propietario otorgado al momento de la entrega de su departamento se especifica el correcto uso y mantenimiento preventivo que se le debe realizar a sus instalaciones a fin de evitar que estas fallen por el propio uso que provoca el desgaste natural.  Puede encontrar el Manual en nuestro portal de post-venta en línea. Link a Manual.<p>
+Es importante precisar que en caso se presentara alguna solicitud, observación y/o requerimiento adicional tras el arreglo debe llenar un nuevo reclamo en nuestro portal de post-venta en línea o comunicarte a nuestro Call Center de Atención al Cliente 206-7270. Este es el único mecanismo que garantiza la atención de su solicitud de post-venta, cualquier otra forma de solicitud no será atendida.<p>
+Quedamos como siempre a su disposición si tiene alguna consulta o solicitud adicional.<p>
+Atentamente';
+ 		
+ 		/*'html' => '<p>FELICIDADES!!!</p><p>Ganaste el tema'.$topic.' ve a nuestro menú de temas y sigue participando</p>',*/
+ 		$message = array(
+ 		    'subject' => $subject,
+ 		    'from_email' => $from,
+ 		    'html' => $text,
+ 		    'to' => array(array('email' => $to, 'name' => $name)),
+ 		    'merge_vars' => array(array(
+	 		        'rcpt' => 'recipient1@domain.com',
+	 		        'vars' =>
+	 		        array(
+	 		            array(
+	 		                'name' => 'FIRSTNAME',
+	 		                'content' => 'Recipient 1 first name'),
+	 		            array(
+	 		                'name' => 'LASTNAME',
+	 		                'content' => 'Last name')
+	 		    ))));
+
+ 		$template_name = 'test';
+
+ 		$template_content = array(
+ 		    array(
+ 		        'name' => 'main',
+ 		        'content' => 'Hi *|FIRSTNAME|* *|LASTNAME|*, thanks for signing up.'),
+ 		    array(
+ 		        'name' => 'footer',
+ 		        'content' => 'Copyright 2012.')
+
+ 		);
+		$mandrill->messages->sendTemplate($template_name, $template_content, $message);
+		return "";
+	}
+
 	function send_email_viva_approve_fix(){
 		global $TMPL;
 		$this->EE =& get_instance(); // EEv2 syntax
