a7e9342155d8c46a425a94cbde0b734fe91c9464
Ricardo Diaz <ricardotk002@gmail.com>
2014-08-08 11:04:14 -0500

    endpoint avance

--- system/expressionengine/third_party/endpoint/pi.endpoint.php
+++ system/expressionengine/third_party/endpoint/pi.endpoint.php
@@ -12,11 +12,10 @@ class endpoint
         $this->EE =& get_instance(); // EEv2 syntax
         $TMPL = $this->EE->TMPL;
 
-        ee()->db->distinct('marca');
-        ee()->db->select('marca');
-        $query = ee()->db->get_where('exp_freeform_form_entries_4', array('form_field_19' => 3));
+        $query = ee()->db
+                    ->get_where('exp_eventos_calendario');
 
-        return $query->result();
+        return json_encode($query->result());
     }
 }
 
