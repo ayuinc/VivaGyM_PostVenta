ba889c1ae2481114b45825d0db8d2be2bb311f6f
Ricardo Diaz <ricardotk002@gmail.com>
2014-08-08 15:37:53 -0500

    fixes on endpoints

--- system/expressionengine/third_party/endpoint/pi.endpoint.php
+++ system/expressionengine/third_party/endpoint/pi.endpoint.php
@@ -6,14 +6,14 @@ class endpoint
         $this->EE =& get_instance();
     }
 
-    public function inspecciones()
+    public function obtener_eventos()
     {
         global $TMPL;
         $this->EE =& get_instance(); // EEv2 syntax
         $TMPL = $this->EE->TMPL;
 
         $tipo_evento = ee()->TMPL->fetch_param('tipo_evento');
-        ee()->db->where('id_event_type',$tipo_evento);
+        ee()->db->where('tipo_evento_id',$tipo_evento);
         $query = ee()->db->get('exp_eventos_calendario');
 
         $response = array();
