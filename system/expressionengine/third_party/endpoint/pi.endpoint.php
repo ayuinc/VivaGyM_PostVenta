656e15d143713be3ce688f9a27ec78917a9fba47
Ricardo Diaz <ricardotk002@gmail.com>
2014-08-08 17:36:57 -0500

    endpoints

--- system/expressionengine/third_party/endpoint/pi.endpoint.php
+++ system/expressionengine/third_party/endpoint/pi.endpoint.php
@@ -13,12 +13,29 @@ class endpoint
         $TMPL = $this->EE->TMPL;
 
         $tipo_evento = ee()->TMPL->fetch_param('tipo_evento');
-        ee()->db->where('tipo_evento_id',$tipo_evento);
+        ee()->db->where('tipo_evento_id', $tipo_evento);
         $query = ee()->db->get('exp_eventos_calendario');
 
-        $response = array();
+        $response = array("resultados" => $query->result());
 
-        return json_encode($query->result());
+        return json_encode($response);
+    }
+
+    public function mis_eventos()
+    {
+        global $TMPL;
+        $this->EE =& get_instance(); // EEv2 syntax
+        $TMPL = $this->EE->TMPL;
+
+        $tipo_evento = ee()->TMPL->fetch_param('tipo_evento');
+        $member_id = ee()->TMPL->fetch_param('member_id');
+        ee()->db->where('tipo_evento_id', $tipo_evento);
+        ee()->db->where('encargado_id', $member_id);
+        $query = ee()->db->get('exp_eventos_calendario');
+
+        $response = array("resultados" => $query->result());
+
+        return json_encode($response);
     }
 }
 
