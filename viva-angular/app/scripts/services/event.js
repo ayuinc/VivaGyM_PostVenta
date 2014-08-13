33337994e2c12235894315c053e06c4b13f44af0
Freddy Rangel <frederick.rangel@gmail.com>
2014-08-13 15:06:14 -0500

    changed $q to $http

--- viva-angular/app/scripts/services/event.js
+++ viva-angular/app/scripts/services/event.js
@@ -11,18 +11,28 @@ app.service('Event', ['$http', '$q', function($http, $q) {
 
   inspectionUrl = 'http://162.243.222.54/calendario/propietario/inspecciones';
 
-  return {
-    unbookedInspections: function() {
-      return $http.get(inspectionUrl)
-        .then(function(response){
-    	  if(typeof response.data === 'object') {
-	    return response.data;
-          } else {
-            return $q.reject(response.data);
-	  }
-        }, function(response){
-	  return $q.reject(response.data);
-	});
-    }
-  }
+  var getJson = function(calendar_url) {
+    $http.get(calendar_url)
+      .success(function(data) {
+        return data;
+      });
+  };
+
+  this.unbookedInspections = getJson(inspectionUrl);
+
+
+  //return {
+    //unbookedInspections: function() {
+      //return $http.get(inspectionUrl)
+        //.then(function(response){
+        //if(typeof response.data === 'object') {
+			//return response.data;
+          //} else {
+            //return $q.reject(response.data);
+		//}
+        //}, function(response){
+		//return $q.reject(response.data);
+	//});
+    //}
+  //}
 }]);
